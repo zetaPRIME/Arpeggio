e99042be79c4d04bd775b81e3a3238219ee83b0c
zetaPRIME <qmanxt@gmail.com>
2022-02-14 04:48:57 -0500

    oops

--- src/main/java/net/zetaprime/arpeggio/items/FirearmItem.java
+++ src/main/java/net/zetaprime/arpeggio/items/FirearmItem.java
@@ -1,6 +1,7 @@
 package net.zetaprime.arpeggio.items;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
